0eddbd165de642e80886234298b6f25b97377888
Timur <timur.salakhetdinov@gmail.com>
2017-09-17 22:11:43 +0300

    clean code

--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -5,25 +5,25 @@ import static org.junit.Assert.assertEquals;
 public class CounterTest {
     @Test
     public void countOne() throws Exception {
-        assertEquals(0, count(1));
+        assertEquals(0, countFactorTwoInFactorialOf(1));
     }
 
     @Test
     public void countTwo() throws Exception {
-        assertEquals(1, count(2));
+        assertEquals(1, countFactorTwoInFactorialOf(2));
     }
 
     @Test
     public void countFour() throws Exception {
-        assertEquals(3, count(4));
+        assertEquals(3, countFactorTwoInFactorialOf(4));
     }
 
     @Test
     public void countEight() throws Exception {
-        assertEquals(7, count(8));
+        assertEquals(7, countFactorTwoInFactorialOf(8));
     }
 
-    private int count(int n) {
+    private int countFactorTwoInFactorialOf(int n) {
         return n/8 + n/4 + n/2;
     }
 }
