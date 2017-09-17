ea4945a63046d990dbf66caf21825ba2cb1004cb
Timur <timur.salakhetdinov@gmail.com>
2017-09-17 23:07:16 +0300

    add acceptance test

--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -1,10 +1,29 @@
 import org.junit.Test;
 
+import java.math.BigInteger;
+
 import static org.junit.Assert.assertEquals;
 
 public class CounterTest {
     private void assertsNumberOfFactorTwoInFactorialOf(int n, int nOfFactorTwo) {
         assertEquals(nOfFactorTwo, countFactorTwoInFactorialOf(n));
+    }
+
+    private int bruteForceCountFactorTwoInFactorialOf(int n) {
+        BigInteger fact = fact(new BigInteger(Integer.toString(n)));
+        int i = 0;
+        while (fact.remainder(new BigInteger("2")).compareTo(new BigInteger("0")) == 0
+                && fact.divide(new BigInteger("2")).compareTo(new BigInteger("0")) == 1) {
+            fact = fact.divide(new BigInteger("2"));
+            i++;
+        }
+        return i;
+    }
+
+    private BigInteger fact(BigInteger n) {
+        if (n.compareTo(new BigInteger("1")) == 0)
+            return new BigInteger("1");
+        return n.multiply(fact(n.subtract(new BigInteger("1"))));
     }
 
     @Test
@@ -30,6 +49,12 @@ public class CounterTest {
     @Test
     public void sixteen() throws Exception {
         assertsNumberOfFactorTwoInFactorialOf(16, 15);
+    }
+
+    @Test
+    public void acceptance() throws Exception {
+        int n = 100;
+        assertEquals(bruteForceCountFactorTwoInFactorialOf(n), countFactorTwoInFactorialOf(n));
     }
 
     private int countFactorTwoInFactorialOf(int n) {
