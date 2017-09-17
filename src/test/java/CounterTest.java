cb2f6abd2f4579fd93b8a05f65404b1e56b112f6
Timur <timur.salakhetdinov@gmail.com>
2017-09-17 21:47:19 +0300

    add case of n = 1

---
+++ src/test/java/CounterTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CounterTest {
+    @Test
+    public void countOne() throws Exception {
+        assertEquals(0, count(1));
+    }
+
+    private int count(int n) {
+        return 0;
+    }
+}
